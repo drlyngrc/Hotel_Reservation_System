48a58aea96f8c7cf9e0a247e0b8b0f00e3a0167f
hishiro <123607245+drlyngrc@users.noreply.github.com>
2023-07-17 12:13:51 +0800

    Convert int option into char option

--- FINAL_PROJECT.cpp
+++ FINAL_PROJECT.cpp
@@ -15,48 +15,48 @@ using namespace std;
 // Function to display the admin menu
 void displayAdminMenu() {
     cout << "=== Admin Menu ===" << endl;
-    cout << "1. Manage reservation schedules" << endl;
-    cout << "2. Client account management" << endl;
-    cout << "3. Exit" << endl;
+    cout << "[a] Manage reservation schedules" << endl;
+    cout << "[b] Client account management" << endl;
+    cout << "[c] Exit" << endl;
     cout << "Enter your choice: ";
 }
 
 // Function to display the client menu
 void displayClientMenu() {
     cout << "=== Client Menu ===" << endl;
-    cout << "1. Room Reservation" << endl;
-    cout << "2. View bookings" << endl;
-    cout << "3. Account settings" << endl;
-    cout << "4. Log Out" << endl;
-    cout << "5. Exit" << endl;
+    cout << "[a] Room Reservation" << endl;
+    cout << "[b] View bookings" << endl;
+    cout << "[c] Account settings" << endl;
+    cout << "[d] Log Out" << endl;
+    cout << "[e] Exit" << endl;
     cout << "Enter your choice: ";
 }
 
 void RoomReserve(){
 	cout << "=== Room Reservation ===" << endl;
-    cout << "1. Choose a schedule" << endl;
-    cout << "2. View room types and amenities" << endl;
-    cout << "3. Back" << endl;
-    cout << "4. Exit" << endl;
+    cout << "[a] Choose a schedule" << endl;
+    cout << "[b] View room types and amenities" << endl;
+    cout << "[c] Back" << endl;
+    cout << "[d] Exit" << endl;
     cout << "Enter your choice: ";
 }
 
 void Reservation(HotelReservationSystem& sys){
-	int choice;
+	char choice;
 	do{
 		system("cls");
 		RoomReserve();
 		cin >> choice;
 		switch(choice){
-			case 1:
+			case 'a':
 				//choose a schedule
 				break;
-			case 2:
+			case 'b':
 				//view room type
 				char choice;
 				system("cls");
 				displayRoomInformation();
-				cout << "[a]Back     [0]Exit: ";
+				cout << "[a]Back     [b]Exit: ";
 				cin >> choice;
 				if(choice == 'a'){
 					break;
@@ -64,68 +64,68 @@ void Reservation(HotelReservationSystem& sys){
 					exit(0);
 				}
 				break;
-			case 3:
+			case 'c':
 				return;
-			case 4:
+			case 'd':
 				exit(0);
 			default:
 				cout << "Invalid choice. Please try again." << endl;
 		}
-	} while(choice != 4);
+	} while(choice != 'd');
 }
 
 // Function to handle the main admin menu
 void AdminMenu(HotelReservationSystem& sys) {
 
-    int choice;
+    char choice;
     do {
     	system("cls");
         displayAdminMenu();
         cin >> choice;
         switch (choice) {
-            case 1:
+            case 'a':
                 // Your code for managing reservation schedules here
                 break;
-            case 2:
+            case 'b':
                 // Your code for client account management here
                 break;
-            case 3:
+            case 'c':
                 cout << "Exiting..." << endl;
                 break;
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
-    } while (choice != 3);
+    } while (choice != 'c');
 }
 
 // Function to handle the main client menu
 void ClientMenu(HotelReservationSystem& sys) {
-    int choice;
+    char choice;
     do {
     	system("cls");
         displayClientMenu();
         cin >> choice;
         switch (choice) {
-            case 1:
+            case 'a':
                 Reservation(sys); 
                 break;
-            case 2:
+            case 'b':
                 // Your code for viewing bookings here
                 break;
-            case 3:
+            case 'c':
                 // Your code for account settings here
                 Account_setting(sys);
                 break;
-            case 4:
+            case 'd':
             	cout << "Logging out..." << endl;
             	break;
-			case 5:
+			case 'e':
 				cout << "Exiting..." << endl;
                 exit(0);
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
-    } while (choice != 4);
+    } while (choice != 'd');
 }
 
 // Function to login
@@ -133,19 +133,19 @@ void showLoginPage(HotelReservationSystem& sys){
     Account account;
     system("cls");
     cout << "Login System" << endl;
-    int choice;
-    cout << "1. Register" << endl;
-    cout << "2. Login" << endl;
-    cout << "3. Exit" << endl;
+    char choice;
+    cout << "[a] Register" << endl;
+    cout << "[b] Login" << endl;
+    cout << "[c] Exit" << endl;
     cout << "Enter your choice: ";
     cin >> choice;
 
     switch (choice) {
-        case 1:
+        case 'a':
             account.registerUser();
             system("pause");
             break;
-        case 2: {
+        case 'b': {
             string loggedInUsername = account.loginUser();
             if (!loggedInUsername.empty()) {
                 if (loggedInUsername == "admin") {
@@ -153,7 +153,6 @@ void showLoginPage(HotelReservationSystem& sys){
                     system("pause");
                     AdminMenu(sys);
                 } else {
-                    //cout << "Logged-in username: " << loggedInUsername << endl;
                     cout << "Logged in successfully." << endl;
 					system("pause");
                     ClientMenu(sys);
@@ -162,7 +161,7 @@ void showLoginPage(HotelReservationSystem& sys){
             system("pause");
             break;
         }
-        case 3:
+        case 'c':
             cout << "Exiting..." << endl;
             system("pause");
             exit(0);
