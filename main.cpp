1a0074b9746c20f79515d8d5367982af1bfa01f1
hishiro <123607245+drlyngrc@users.noreply.github.com>
2023-07-20 17:05:30 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -16,11 +16,11 @@ using namespace std;
 
 // Function to display the admin menu
 void displayAdminMenu() {
-	
-    cout << "=== Admin Menu ===" << endl;
-    cout << "[a] Manage reservation schedules" << endl;
-    cout << "[b] Exit" << endl;
-    cout << "Enter your choice: ";
+    cout << "=== Manage Reservation Schedule === " << endl;
+    cout << "[a] Display reservation/s" << endl;
+    cout << "[b] Confirm reservation/s" << endl;
+    cout << "[c] Back" << endl;
+    cout << "Enter your choice: " << endl;
 }
 
 // Function to display the client menu
@@ -38,8 +38,9 @@ void RoomReserve(){
 	cout << "=== Room Reservation ===" << endl;
     cout << "[a] Schedule Room Reservation" << endl;
     cout << "[b] View room types and amenities" << endl;
-    cout << "[c] Back" << endl;
-    cout << "[d] Exit" << endl;
+    cout << "[c] Show available rooms" << endl;
+    cout << "[d] Back" << endl;
+    cout << "[e] Exit" << endl;
     cout << "Enter your choice: ";
 }
 
@@ -57,17 +58,38 @@ void Reservation(HotelReservationSystem& sys){
 				char choice;
 				system("cls");
 				displayRoomInformation();
-				cout << "[a]Back     [b]Exit: ";
-				cin >> choice;
-				if(choice == 'a'){
-					break;
-				} else if (choice == '0'){
-					exit(0);
+				while(true){
+					cout << "[a]Back: ";
+					cin >> choice;
+					if(choice == 'a'){
+						break;
+					} else{
+						cout << "Invalid. Try again." << endl;
+					}
 				}
 				break;
 			case 'c':
+				int chosenMonth, chosenFromDate;
+	            cout << "Enter the month number (1-12): ";
+	            cin >> chosenMonth;
+	            cout << "Enter the date (1-" << calendar.getMonthDays(chosenMonth, 2024) << "): ";
+	            cin >> chosenFromDate;
+	            DisplayRoomAvailability(chosenMonth, chosenFromDate);
+	            while(true){
+	            	cout << endl;
+					cout << "[a]Back: ";
+					cin >> choice;
+					if(choice == 'a'){
+						break;
+					} else{
+						cout << "Invalid. Try again." << endl;
+					}
+				}
+				system("pause");
+	            break;
+	        case 'd':
 				return;
-			case 'd':
+			case 'e':
 				exit(0);
 			default:
 				cout << "Invalid choice. Please try again." << endl;
@@ -85,37 +107,17 @@ void AdminMenu(HotelReservationSystem& sys) {
         cin >> choice;
         switch (choice) {
             case 'a':
-            	char option;
-            	system("cls");
-                cout << "=== Manage Reservation Schedule === " << endl;
-                cout << "[a] Display reservation/s" << endl;
-                cout << "[b] Mark date as Unavailable" << endl;
-                cout << "[c] Mark date as Available" << endl;
-                cout << "[d] Back" << endl;
-                cout << "Enter your choice: " << endl;
-                cin >> option;
-                switch(option){
-                	case 'a':
-                		Display_reservation_admin();
-                		break;
-                	case 'b':
-                		markDateUnavailableMenu();
-                		break;
-                	case 'c':
-                		markDateAvailableMenu();
-                		break;
-                	case 'd':
-                		cout << "Exiting..." << endl;
-                		break;
-                	default:
-                		cout << "Invalid choice. Please try again." << endl;
-				}
-                break;
+            	Display_reservation();
+            	break;
             case 'b':
+            	confirmSelectedReservation();
+            	system("pause");
+            	break;
+            case 'c':
                 cout << "Exiting..." << endl;
-                return;
+          		break;
             default:
-                cout << "Invalid choice. Please try again." << endl;
+              	cout << "Invalid choice. Please try again." << endl;
         }
     } while (choice != 'c');
 }
@@ -124,7 +126,7 @@ void ClientMenu(HotelReservationSystem& sys, const string& loggedInUsername) {
     char choice;
     do {
         system("cls");
-        loadReservationsFromFile();
+        //loadReservationsFromFile();
         displayClientMenu();
         cin >> choice;
         switch (choice) {
@@ -132,7 +134,7 @@ void ClientMenu(HotelReservationSystem& sys, const string& loggedInUsername) {
                 Reservation(sys); 
                 break;
             case 'b':
-                Display_reservation_client();
+                Display_reservation();
                 break;
             case 'c':
                 Account_setting(sys, loggedInUsername);
@@ -146,7 +148,7 @@ void ClientMenu(HotelReservationSystem& sys, const string& loggedInUsername) {
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
-        saveReservationsToFile();
+        //saveReservationsToFile();
     } while (choice != 'd');
 }
 
@@ -197,10 +199,11 @@ void showLoginPage(HotelReservationSystem& sys){
 
 int main() {
     HotelReservationSystem sys;
-    
+    loadReservationsFromFile();
     while (true) {
         showLoginPage(sys);
+        saveReservationsToFile();
     }
-
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
     return 0;
 }
